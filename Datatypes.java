b94a6e73c63aaf61226e2b0f5dcd73aed8f83e08
dhanushree-coder <dhanushree08180@gmail.com>
2025-03-19 20:48:48 +0530

    Session 1

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+package programs;
+
+public class Datatypes {
+     public static void main(String[] args) {
+		int age=23;
+		char initial='M';
+		float f=93.45431f;
+		double d= 32.89078;
+		
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 System.out.println(age);
+    	 System.out.println(initial);
+    	 System.out.println(f);
+    	 System.out.println(d);
+
+     }
+}
---
+++ PersonalInfo.java
@@ -0,0 +1,20 @@
+package programs;
+
+import java.util.Scanner;
+
+public class PersonalInfo {
+	public static void main(String[] args) {
+		Scanner obj =new Scanner(System.in);
+		
+		System.out.println("Enter your mobile number:");
+int mob =obj.nextInt();
+
+	System.out.println("enter ur name");
+	String name =obj.next();
+	
+	System.out.println("Enter ur address");
+	String addr =obj.next();
+	
+	}
+
+}
