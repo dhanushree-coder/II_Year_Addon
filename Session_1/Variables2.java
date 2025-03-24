26dad9f0d0b7ef81d1e0452f18b6bb8c1134a662
dhanushree-coder <dhanushree08180@gmail.com>
2025-03-24 11:39:58 +0530

    Add files via upload

---
+++ Session_1/Variables2.java
@@ -0,0 +1,18 @@
+package programs;
+
+public class Variables2 {
+
+	static void show()
+	{
+		System.out.println("methoda from variables 2 class");
+	}
+	
+	
+	void students()
+	{
+		System.out.println("information of students");
+	}
+	
+
+	
+}
